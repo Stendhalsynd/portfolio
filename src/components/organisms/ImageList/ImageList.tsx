838888361a5b063ed5f01697548bf7b477553d48
Stendhalsynd <qmflf556@naver.com>
2023-12-18 08:12:49 +0900

    feat/ 기술 스택 업데이트

--- src/components/organisms/ImageList/ImageList.tsx
+++ src/components/organisms/ImageList/ImageList.tsx
@@ -13,8 +13,18 @@ import js from "@image/img_javascript.png"
 import ts from "@image/img_typescript.png"
 import sass from "@image/img_sass.png"
 import redux from "@image/img_redux.png"
-import react from "@image/img_react.png"
-import recoil from "@image/img_recoil.png"
+import react from "@image/img_react.svg"
+import recoil from "@image/img_recoil.svg"
+import next from "@image/img_next.svg"
+import reacthookform from "@image/img_react_hook_form.svg"
+import eslint from "@image/img_eslint.svg"
+import prettier from "@image/img_prettier.svg"
+import storybook from "@image/img_storybook.svg"
+import chromatic from "@image/img_chromatic.svg"
+import styledcomponent from "@image/img_styled_component.svg"
+import openvidu from "@image/img_openvidu.png"
+import mui from "@image/img_mui.png"
+import webrtc from "@image/img_webrtc.png"
 
 import python from "@image/img_python.png"
 import java from "@image/img_java.png"
@@ -23,15 +33,17 @@ import nest from "@image/img_nestjs.png"
 import sequelize from "@image/img_sequelize.png"
 import gorm from "@image/img_gorm.png"
 import grpc from "@image/img_grpc.png"
-import typeorm from "@image/img_typeorm.png"
-import springboot from "@image/img_springboot.png"
-import mysql from "@image/img_mysql.png"
+import typeorm from "@image/img_typeorm.svg"
+import springboot from "@image/img_springboot.svg"
+import mysql from "@image/img_mysql.svg"
 
 import ec2 from "@image/img_ec2.png"
 import docker from "@image/img_docker.png"
-import pm2 from "@image/img_pm2.png"
-import netlify from "@image/img_netlify.png"
+import pm2 from "@image/img_pm2.svg"
+import netlify from "@image/img_netlify.svg"
 import gitlab_cicd from "@image/img_gitlab_cicd.png"
+import github_action from "@image/img_github_action.svg"
+import vercel from "@image/img_vercel.svg"
 
 import fork from "@image/img_fork.png"
 import git from "@image/img_git.png"
@@ -78,6 +90,16 @@ const ImageList: React.FC<ImageListProps> = ({ images, variant }) => {
         redux,
         react,
         recoil,
+        next,
+        reacthookform,
+        eslint,
+        prettier,
+        storybook,
+        chromatic,
+        styledcomponent,
+        openvidu,
+        mui,
+        webrtc,
       }
       break
     case "backend":
@@ -101,6 +123,8 @@ const ImageList: React.FC<ImageListProps> = ({ images, variant }) => {
         pm2,
         netlify,
         gitlab_cicd,
+        github_action,
+        vercel,
       }
       break
     case "versioncontrol":
